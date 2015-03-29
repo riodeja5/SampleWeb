364c434911d306d61b5eecdac1feaa0ea6e1b181
S.Sumida <riodeja.5.shota@gmail.com>
2015-03-30 00:13:00 +0900

    動的View更新のお試し

---
+++ SumidaWeb/Controllers/IssueController.cs
@@ -0,0 +1,30 @@
+﻿using SumidaWeb.Models;
+using System.Web.Mvc;
+using System.Linq;
+
+
+namespace SumidaWeb.Controllers
+{
+    public class IssueController : Controller
+    {
+        private SumidaWebContext db = new SumidaWebContext();
+        // GET: Issue
+        public ActionResult Index()
+        {
+            ViewBag.Id = new SelectList(db.Users, "Id", "UserName");
+            return View();
+        }
+
+        public ActionResult FabSearch(User user)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var fabs = db.Fabs
+                    .Where(f => f.UserID == user.Id)
+                    .OrderBy(f => f.Id);
+                return PartialView("_FabSearch", fabs);
+            }
+            return Content("Ajax通信以外のアクセスはできません。");
+        }
+    }
+}
